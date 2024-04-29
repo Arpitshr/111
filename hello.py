2a958afc2278271601e4b7b064dad249782fbfd7
Arpitshr <arpitshrivastavaaa@gmail.com>
2024-04-29 19:47:27 +0530

    2nd commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
+
+from flask import Flask
+import psutil
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+@app.route('/showp')
+def show_p():
+    pids = psutil.pids()
+    process= {}
+    for pid in pids:
+        p = psutil.Process(pid)
+        process.update({pid:p.name()})
+    return process
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
+
